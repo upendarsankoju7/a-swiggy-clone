fdcca09a47723d68726b78416b34f63e38dd0eff
upendarsankoju7 <50440972+upendarsankoju7@users.noreply.github.com>
2023-12-09 18:55:22 +0530

    Update Footer.jsx
    Updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -44,7 +44,7 @@ function Footer() {
                     <p>Hyderabad</p>
                     <p>Nakerkal</p>
                     <p>Mumbai</p>
-                    <p>Pune </p>
+                    <p>Warngal </p>
                     <button style={{padding:"10px",background:"transparent",color:"#9e9e9e",border:"1px solid rgba(196, 198, 202, 0.1)",borderRadius:"20px"}}>589 cities    <i className="ms-5 fa-solid fa-angle-down"></i></button>
                 </div>
             </div>
